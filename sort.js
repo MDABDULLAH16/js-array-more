46ca3c31493af3d676f79a121c8c5ea59cb697ef
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-06 23:32:17 +0600

    simple sort array with number

---
+++ sort.js
@@ -0,0 +1,21 @@
+const numbers = [23, 6, 7, 8, 5, 1, 2, 45, 76];
+// with Array function ascending
+// numbers.sort((a, b) => a - b);
+// // console.log(numbers);
+
+// // with Array function descending
+// numbers.sort(function (a, b) {
+//   return b - a;
+// });
+
+// console.log(numbers);
+// const sort = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   const firstNum = sort.pop();
+//   console.log(firstNum);
+
+//   const arrayNum = numbers[i];
+//   // console.log(arrayNum);
+// }
+// console.log(sort);
