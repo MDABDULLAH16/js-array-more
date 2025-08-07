2844ea8b9c50389ff600de7d7dbb2e331d043e60
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-07 11:04:27 +0600

    task-4 reverse word

---
+++ task-4.js
@@ -0,0 +1,18 @@
+// Task 4 (Hard)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+// Input: const statement = 'I am a hard working person'
+
+// Output:
+
+// 'person working hard a am I'
+
+const statement = "I am a hard working person";
+const reversedArray = statement.split(" ");
+console.log(reversedArray);
+
+let reversedStatement = "";
+for (const word of reversedArray) {
+  reversedStatement = word + " " + reversedStatement;
+}
+console.log(reversedStatement);
