11821b07927a4032e065d6de6be2647db3f0aa7e
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-07 11:21:53 +0600

    matrix array update

---
+++ task-7.js
@@ -0,0 +1,22 @@
+// Task 7
+// Given a 2D array, update the value at second row first item to 99 and print the updated array.
+// input:[
+//   [1, 2],
+//   [3, 4],
+//   [5, 6]
+// ]
+// Expected Array:
+// [
+//   [1, 2],
+//   [99, 4],
+//   [5, 6]
+// ]
+
+const arrayOfArray = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const updatedArray = (arrayOfArray[1][0] = 99);
+console.log(updatedArray);
+console.log(arrayOfArray);
